cfb942fc282b9f4580a932bdd4f4501c20015739
Rex Tan <tan.rexc@gmail.com>
2020-01-23 05:18:43 +0800

    Add logo

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Image } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import QuestionScreen from './activity/question'
+import Logo from './assets/start-logo.png'
 
 class HomeScreen extends React.Component {
   render() {
     const {navigate} = this.props.navigation;
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button title="Start Survey" onPress={() => navigate('Question', {name : 'Rex'})}/>
+        <Image source={require('./assets/start-logo.png')} style={{height : 150, width: 150, marginBottom: 100}}/>
+        <Button color="#e74c3c" title="Start Survey" onPress={() => navigate('Question', {name : 'Rex'})}/>
       </View>
     );
   }
@@ -19,6 +20,9 @@ class HomeScreen extends React.Component {
 const AppNavigator = createStackNavigator({
   Home: {
     screen: HomeScreen,
+    navigationOptions: {
+      headerShown : false
+    }
   },
   Question : {
     screen : QuestionScreen,
