7e5456656e5abe2157f256de308ac22728f8850d
Jobstream Corp <rex@jobstreamapp.com>
2020-02-14 16:33:01 +0800

    asdas

--- components/smsSender.js
+++ components/smsSender.js
@@ -1,12 +1,12 @@
-var smsGateway = require('sms-gateway-nodejs')('regiebadion361@gmail.com', '6fe6465f7f')
+// var smsGateway = require('sms-gateway-nodejs')('regiebadion361@gmail.com', '6fe6465f7f')
 
 export function sendSMS (mobile, code) {
-  smsGateway.message.sendMessageToNumber('115489', mobile, 'STI Evaluation Verification Code : ' + code)
-  .then((response) => {
-    // do something with response
-  })
-  .catch((error) => {
-    // handle error
-  })
+  // smsGateway.message.sendMessageToNumber('115489', mobile, 'STI Evaluation Verification Code : ' + code)
+  // .then((response) => {
+  //   // do something with response
+  // })
+  // .catch((error) => {
+  //   // handle error
+  // })
 
 }
